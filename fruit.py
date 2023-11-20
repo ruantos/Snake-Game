b17c4e6e07f897c7c8586aad1300ecbfdf5c00a4
Juan Oliveira <juanoliveira@pop-os.localdomain>
2023-11-19 22:38:04 -0300

    fail

---
+++ fruit.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+from random import randint
+
+class Fruit(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("black")
+        self.speed("fastest")
+        self.move()
+
+    def move(self):
+        self.goto(x=randint(-300, 300),
+                y=randint(-300, 300))
