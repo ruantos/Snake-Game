24db20ad006eb02f050d25e7b81d21bd0eac8667
Juan Oliveira <juanoliveira@pop-os.localdomain>
2023-11-21 13:38:36 -0300

    high score and over class

--- screen.py
+++ screen.py
@@ -1,33 +1,51 @@
 from turtle import Screen, Turtle
+import time
 
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
         self.pontos = 0
+        self.max_pontos = 0
         self.hideturtle()
         self.penup()
         self.goto(0, 300)
-        self.write(arg=f"Pontuação: {self.pontos}",
+        self.refresh()
+
+
+    def refresh(self):
+        self.write(arg=f"Pontuação: {self.pontos}    Max: {self.max_pontos}",
                 move=False,
                 align="Center",
                 font=("Arial", 14, "bold"))
+        
     def add(self):
         self.pontos += 1
         self.undo()
-        self.write(arg=f"Pontuação: {self.pontos}",
-                move=False,
-                align="Center",
-                font=("Arial", 14, "bold"))
-        
-    def over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER",
-                move=False,
-                align="Center",
-                font=("Arial", 18, "bold"))
+        self.refresh()
+
+    def reset(self):
+        if self.pontos > self.max_pontos:
+            self.max_pontos = self.pontos
+        self.undo()
+        self.pontos = 0
+        self.refresh()
 
         
+class Over(Turtle):
+    def __init__(self):
+        self.goto(0, 0)
+        self.penup()
+        self.hideturtle()
+
+    def over(self, pontos):
+        self.write(arg=f"GAME OVER\n\nSUA PONTUAÇÃO: {pontos}",
+                move=False,
+                align="Center",
+                font=("Arial", 20, "bold"))
+        time.sleep(3)
+        self.undo()
+
 def Tela(title="Jogo da cobrinha",
     width=700,
     heigth=700,
